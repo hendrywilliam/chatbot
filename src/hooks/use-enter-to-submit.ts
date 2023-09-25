ef040205148db9274a50e020c61022ec8a80c93d
hendrywilliam <hendriwilliam29@gmail.com>
2023-09-25 11:27:49 +0700

    updating dependencies

--- src/hooks/use-enter-to-submit.ts
+++ src/hooks/use-enter-to-submit.ts
@@ -11,7 +11,6 @@ export function useEnterToSubmit(
     if (submitter.current?.disabled) {
       return;
     }
-
     if (event.key === key && event.ctrlKey) {
       event.preventDefault();
       target?.current?.requestSubmit(submitter.current);
