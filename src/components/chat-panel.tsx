f9a46407f5c6e6f911d5c464e51de6f1c56e326f
unknown <hendriwilliam29@gmail.com>
2024-07-05 18:15:56 +0700

    Remove stop generating button

--- src/components/chat-panel.tsx
+++ src/components/chat-panel.tsx
@@ -2,8 +2,6 @@
 
 import PromptForm from "@/components/prompt-form";
 import { UseChatHelpers } from "@/types";
-import { Button } from "@/components/ui/button";
-import { StopIcon } from "@/components/ui/icons";
 
 interface ChatPanelProps
     extends Pick<
@@ -27,22 +25,10 @@ export default function ChatPanel({
     isLoading,
     triggerStop,
     clearChats,
-    regenerateResponse,
-    messages,
     clearInput,
 }: ChatPanelProps) {
     return (
         <div className="fixed inset-x-0 bottom-0  mx-auto w-[95%] rounded-t border bg-background p-4 lg:w-[35%]">
-            {isLoading && (
-                <Button
-                    variant="outline"
-                    className="absolute inset-x-0 -top-12 mx-auto w-max gap-1 text-xs text-muted-foreground drop-shadow-none"
-                    onClick={(e) => triggerStop(e)}
-                >
-                    <StopIcon />
-                    Stop Generating
-                </Button>
-            )}
             <PromptForm
                 setInput={setInput}
                 input={input}
